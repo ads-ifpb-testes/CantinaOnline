c67b495299c2309dd038d031ff7aa0d993489195
LuzEsperanza <github.com/LuzEsperanza>
2019-08-16 14:46:22 -0300

    template

--- cantina_jsf/src/main/java/br/com/ifpb/cantinaonline/model/visao/ImagesView.java
+++ cantina_jsf/src/main/java/br/com/ifpb/cantinaonline/model/visao/ImagesView.java
@@ -1,4 +1,23 @@
 package br.com.ifpb.cantinaonline.model.visao;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+@ManagedBean
 public class ImagesView {
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 2; i <= 5; i++) {
+            images.add("lanche" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
 }
+
